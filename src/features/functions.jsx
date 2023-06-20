6eebd4be8b99d3d9a850490028a3df7585b7de57
mikkom79 <mikkomanninen79@gmail.com>
2023-06-20 15:16:09 +0300

    single name possibility added to email2name parser

--- src/features/functions.jsx
+++ src/features/functions.jsx
@@ -2,6 +2,7 @@ export const fullName = (email) => {
   if (!email) return;
   const _name = email.split("@")[0];
   const name = _name.replace(/[0-9]/g, "");
+  if (!name.includes(".")) return name;
   const names = name.split(".");
   const firstname = names[0].charAt(0).toUpperCase() + names[0].slice(1);
   const lastname = names[names.length - 1].charAt(0).toUpperCase() + names[names.length - 1].slice(1);
